b0de581e40699846cb23ba81436d46322f691649
Gokay Iseri <gokay.iseri@outlook.com>
2024-07-24 17:25:37 +0300

    Last comment

--- Programming Theory Project/Assets/Scripts/GameManager.cs
+++ Programming Theory Project/Assets/Scripts/GameManager.cs
@@ -8,14 +8,15 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
 	[SerializeField] List<GameObject> shapes;
-	Cube cube;
-	Sphere sphere;
-	Capsule capsule;
+	Shape cube;
+	Shape sphere;
+	Shape capsule;
 
 	[SerializeField] TextMeshProUGUI shapeDetailsText;
 
 	void Awake()
 	{
+		// upcast
 		cube = shapes[0].GetComponent<Cube>();
 		sphere = shapes[1].GetComponent<Sphere>();
 		capsule = shapes[2].GetComponent<Capsule>();
